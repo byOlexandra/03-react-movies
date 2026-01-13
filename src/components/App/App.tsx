45c5f584106241d76f7cadf75cc3a6eca8fe616e
devolexa <starovatovasasha@gmail.com>
2026-01-13 09:07:27 +0200

    searchBar.tsx file

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,9 @@
 // import { useState } from 'react'
 import './App.module.css'
+import SearchBar from '../SearchBar/SearchBar'
 
-function App() {
+export default function App() {
+    return (
+        <SearchBar />
+    )
 }
-
-export default App
