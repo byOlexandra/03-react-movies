dbba491d663c079124518f5f5a10bc7888f2f466
devolexa <starovatovasasha@gmail.com>
2026-01-18 10:46:54 +0200

    deleted useless console.log

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -7,38 +7,37 @@ interface SearchBarProps {
 
 export default function SearchBar({ onSubmit }: SearchBarProps) {
     const handleSubmit = (formdata: FormData) => {
-        const movie = formdata.get('query') as string;     
+        const movie = formdata.get('query') as string;
         if (!movie || movie.trim() === '') {
             toast.error('Please enter your search query.');
             return
         }
-        console.log(movie);       
         onSubmit(movie)
     }
 
     return (
         <header className={styles.header}>
             <div className={styles.container}>
-            <a className={styles.link}
-            href="https://www.themoviedb.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-            >
-                Powered by TMDB
-            </a>
-            <form className={styles.form} action={handleSubmit}>
-                <input
-                className={styles.input}
-                type="text"
-                name="query"
-                autoComplete="off"
-                placeholder="Search movies..."
-                autoFocus
-            />
-                <button className={styles.button} type="submit">
-                Search
-                </button>
-            </form>
+                <a className={styles.link}
+                    href="https://www.themoviedb.org/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Powered by TMDB
+                </a>
+                <form className={styles.form} action={handleSubmit}>
+                    <input
+                        className={styles.input}
+                        type="text"
+                        name="query"
+                        autoComplete="off"
+                        placeholder="Search movies..."
+                        autoFocus
+                    />
+                    <button className={styles.button} type="submit">
+                        Search
+                    </button>
+                </form>
             </div>
         </header>
     )
